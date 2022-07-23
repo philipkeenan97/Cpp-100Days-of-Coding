352e156c961f31fb74eb0fcbac1da3d790e6fe07
Philip Keenan <64148378+philipkeenan97@users.noreply.github.com>
2022-07-23 19:04:30 +0100

    Add files via upload
    Initial Files made before making this repository and using it for files.

---
+++ 2- First steps/1- First C++ Program/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Philip" << std::endl;
+    std::cout << "Philip" << std::endl;
+    std::cout << "Philip" << std::endl;
+    std::cout << "Philip" << std::endl;
+    std::cout << "Philip" << std::endl;
+    std::cout << "Philip" << std::endl;
+    std::cout << "Philip" << std::endl;
+    std::cout << "Philip" << std::endl;
+    std::cout << "Philip" << std::endl;
+    std::cout << "Philip" << std::endl;
+
+
+}
---
+++ 2- First steps/3 - Errors & Warnings/main.cpp
@@ -0,0 +1,12 @@
+
+//This brings in the iostream library
+#include <iostream>
+/*
+    This is a multiline comment
+    Very odd
+*/
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
